aaf9ed44a9ec2cf27dbbf16c8e1d254707e1de99
BFIbrahim <ibrahim74543@gmail.com>
2023-03-30 23:38:02 -0700

    componets and blog.json file added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Blog from './Components/Blog/Blog';
+import SideCard from './Components/Blog/side card/SideCard';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='header'>
+
+      </div>
+
+      <div className='main row'>
+        <div className='Blog col-md-8'>
+          <Blog></Blog>
+        </div>
+
+        <div className='sideCard col-md-4'>
+          <SideCard></SideCard>
+        </div>
+
+      </div>
     </div>
   );
 }
---
+++ src/Components/Blog/side card/SideCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SideCard = () => {
+    return (
+        <div>
+            <h1>This Is Side card</h1>
+        </div>
+    );
+};
+
+export default SideCard;
